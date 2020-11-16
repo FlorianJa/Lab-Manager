a47c0e839c5980b940905bb952c706ec03b99de6
akhiln95 <akhilnalliboina@gmail.com>
2020-11-16 18:27:00 +0100

    API views and models

--- labmanager/urls.py
+++ labmanager/urls.py
@@ -1,4 +1,4 @@
-"""LabManager URL Configuration
+"""labmanager URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
--- labmanager/wsgi.py
+++ labmanager/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LabManager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'labmanager.settings')
 
 application = get_wsgi_application()
